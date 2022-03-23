f68eaaeaf7ad85bab824ab8bbf40a38c6dbb535a
Nate Gowins <ngowins@gmail.com>
2022-03-23 11:11:35 -0500

    completed exercise

--- app.js
+++ app.js
@@ -7,13 +7,46 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add  = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(a);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
 
 // YOUR CODE HERE
+console.log(a + str);
+console.log(a + str2);
+console.log(a + isPresent);
+console.log(a + firstName);
+console.log(a + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
 
 // YOUR CODE HERE
+console.log(d == str3);
+console.log(d === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && d >= str3);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
